57809e58c6bbd7a57a448b392b73ea745fd3281a
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2018-09-05 17:35:07 +0200

    Added JavaDoc for the Calculator class.

--- src/main/java/com/example/JenkinsTest/Calculator.java
+++ src/main/java/com/example/JenkinsTest/Calculator.java
@@ -1,6 +1,9 @@
 package com.example.JenkinsTest;
 import org.springframework.stereotype.Service;
 
+ /**
+  * The Base Calculator class.
+  */
 @Service
 public class Calculator {
      int sum(int a, int b) {
