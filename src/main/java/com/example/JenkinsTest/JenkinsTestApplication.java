0889a1c2c8c33b5ff4959f48c17a9b89a5e9f22c
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2018-09-05 16:50:53 +0200

    Added JavaDoc for this class

--- src/main/java/com/example/JenkinsTest/JenkinsTestApplication.java
+++ src/main/java/com/example/JenkinsTest/JenkinsTestApplication.java
@@ -3,10 +3,13 @@ package com.example.JenkinsTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Application.
+ */
 @SpringBootApplication
 public class JenkinsTestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsTestApplication.class, args);
-	}
+     public static void main(String[] args) {
+          SpringApplication.run(JenkinsTestApplication.class, args);
+     }
 }
+
